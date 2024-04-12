00d036ac578b9866885bc154778befadba213f50
Yukihiro <12211343+y-marui@users.noreply.github.com>
2024-04-12 14:19:32 +0900

    Prevention of unintentional overwriting.
    In principle, raise an error if the file exists
    When the overwrite flag is set, the old file moves to the trash
    Also appended to the existing file is implemented

--- matplotlib_extension/pyplot.py
+++ matplotlib_extension/pyplot.py
@@ -2,28 +2,51 @@ import matplotlib.pyplot as plt
 from io import BytesIO, StringIO
 import dill
 import fitz
+from send2trash import send2trash
+from pypdf import PdfWriter
 
-def savefig(fig:plt.figure, filename:str):
+def savefig(fig:plt.figure, filename:Path, mode: str = "x"):
     """Save the current figure to a file of ".plt.pdf" which is PDF file including dill object.
 
     Args:
         filename (str): The name of the file to save the figure to.
     """
-    with BytesIO() as fp_pdf:
-        fig.savefig(fp_pdf, format="pdf")
-        fp_pdf.seek(0)
+    assert mode in ["x", "w", "a"]
 
-        with BytesIO() as fp_dill:
-            dill.dump(fig, fp_dill)
-            fp_dill.seek(0)
+    if isinstance(filename, str):
+        filename = Path(filename) 
+        
+    with PdfWriter() as merger:
+        with BytesIO() as fp_pdf:
+            merger = PdfWriter()
+            
+            if isinstance(filename, Path) and filename.exists():
+                if mode == "x":
+                    raise FileExistsError(f"{filename}")
+                elif mode == "w":
+                    send2trash(filename)
+                elif mode == "a":
+                    merger.append(filename)
+            
+            
+            fig.savefig(fp_pdf, format="pdf")
+            fp_pdf.seek(0)
+    
+            with BytesIO() as fp_dill:
+                dill.dump(fig, fp_dill)
+                fp_dill.seek(0)
+    
+                doc = fitz.open("pdf", fp_pdf) 
+                
+                page = doc[0]
+                
+                file_annotation = page.add_file_annot(None, fp_dill, "fig.dill")
+                
+                doc.save(fp_pdf)
 
-            doc = fitz.open("pdf", fp_pdf) 
-            
-            page = doc[0]
-            
-            file_annotation = page.add_file_annot(None, fp_dill, "fig.dill")
-            
-            doc.save(filename)
+            merger.append(fp_pdf)
+
+            merger.write(filename)
 
 
 def loadfig(filename:str)->plt.figure:
@@ -35,12 +58,17 @@ def loadfig(filename:str)->plt.figure:
     Returns:
         plt.figure: The figure object.
     """
-    with open(filename, "rb") as fp:
+    if isinstance(filename, str):
+        filename = Path(filename) 
+        
+    with filename.open("rb") as fp:
         doc = fitz.open(fp)
-        page = doc[0]
-        for annot in page.annots():
-            if annot.info["content"] == 'fig.dill':
-                fig = dill.loads(annot.get_file())
-                break
-
-        return fig
+        figs = []
+        for page in doc:
+            for annot in page.annots():
+                if annot.info["content"] == 'fig.dill':
+                    fig = dill.loads(annot.get_file())
+                    figs.append(fig)
+                    break
+            
+        return figs
